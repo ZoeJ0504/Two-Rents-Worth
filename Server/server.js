11883d673f963f0dbade42f568c6c178525cd162
Zoe Mendez <107508518+ZoeJ0504@users.noreply.github.com>
2023-09-13 10:44:13 -0700

    Signup and login forms created

--- Server/server.js
+++ Server/server.js
@@ -16,3 +16,7 @@ const db = mysql.createConnection({
     password: process.env.DATABASE_PASSWORD,
     database: process.env.DATABASE
 })
+
+app.listen(8081, () => {
+    console.log("Connected to server")
+})
--- client/src/Pages/Signup.js
+++ client/src/Pages/Signup.js
@@ -1,14 +1,26 @@
-import React from 'react'
+import React from 'react';
+import { useState } from 'react';
+import axios from 'axios'
 
 
 
 function SignUp() {
+    const [signUpInfo, setSignUpInfo] = useState({})
+
+    const handleOnChange = (event) => { setSignUpInfo({ ...signUpInfo, event.target: event.target.value }) }
+
 
     return (
         <div>
-            I am the sign up page
+            <form>
+                <input name="firstname" onChange={(event) => { set(event.target.value) }} />
+                <input name='lastname' onChange={(event) => { set(event.target.value) }} />
+                <input name="email" onChange={(event) => { set(event.target.value) }} />
+                <input name="password" onChange={(event) => { set(event.target.value) }} />
+                <input name="passwordconfirmation" onChange={(event) => { set(event.target.value) }} />
+                <button type='submit'>SignUp</button>
+            </form>
         </div>
     )
-}
-
-export default SignUp;
+};
+export default SignUp; 
